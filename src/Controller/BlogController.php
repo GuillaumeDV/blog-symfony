f8f3a70e1a958cb37b2c88d9d3b9e7a0edc44c55
Guillaume <gdv.delvecchio75@gmail.com>
2020-12-03 17:33:38 +0100

    add form article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,16 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index( ArticleRepository $repo): Response
     {
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
@@ -27,10 +37,30 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/new", name="blog_create")
      */
-    public function show()
+    public function create(Request $request, EntityManagerInterface $manager)
     {
-        return $this->render('blog/show.html.twig');
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                     ->add('title')
+                     ->add('content')
+                     ->add('image')
+                     ->getForm();
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(Article $article)
+    {
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
     }
 }
